08785b6a9ff61b225dd611bae982696e62208ab1
Pavel <pavel.evst@gmail.com>
2014-06-20 15:33:36 +0700

    Add more unittests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Mysql.d
 
-#### mysql library binding. Extraction from https://github.com/adamdruppe/arsd
+### mysql library binding. Extraction from https://github.com/adamdruppe/arsd
 
 Documentation is not ready yet
 
@@ -21,3 +21,12 @@ assert(q_res3.length() == 1);
 assert(q_res3.front()["Tables_in_mysql_d_testing"] == "mysql_d_table");
 
 ```
+
+#### Compiling
+
+* Install dub with `brew install dub` or from here http://code.dlang.org/download
+* Run `dub`
+
+#### Testing
+
+* run `dub test`
--- source/mysql/d.d
+++ source/mysql/d.d
@@ -5,15 +5,22 @@ public import mysql.mysql;
 unittest {
     import std.stdio;
 
+    // Connect to database
     MySql connection1;
     connection1 = new MySql("localhost", "root", "root", "mysql");
-    auto q_res1 = connection1.query("DROP DATABASE IF EXISTS mysql_d_testing");
-    auto q_res2 = connection1.query("CREATE DATABASE mysql_d_testing");
+
+    // drop database if exists
+    connection1.query("DROP DATABASE IF EXISTS mysql_d_testing");
+    // create database
+    connection1.query("CREATE DATABASE mysql_d_testing");
 
     // TODO: fix connection.close()
     //connection1.close();
 
+    // connect to new created database
     MySql connection = new MySql("localhost", "root", "root", "mysql_d_testing");
+
+    // create table
     connection.query("CREATE TABLE mysql_d_table (
         id INT NOT NULL AUTO_INCREMENT,
         name VARCHAR(100),
@@ -21,7 +28,34 @@ unittest {
         PRIMARY KEY (id)
     );");
 
+    // list table
     auto q_res3 = connection.query("show tables;");
     assert(q_res3.length() == 1);
     assert(q_res3.front()["Tables_in_mysql_d_testing"] == "mysql_d_table");
+
+    // escape strings
+    assert(connection.escape("string \"with\" quotes"), "string \\\"with\\\" quotes");
+
+    // insert some
+    connection.query("INSERT INTO mysql_d_table (name, date) values (?, ?);", "Paul", "1989-05-06");
+    assert(connection.lastInsertId == 1);
+
+    // select it
+    auto res = connection.query("select * from mysql_d_table;");
+    assert(res.length == 1);
+    assert(res.empty == false);
+
+    assert(res.getFieldIndex("date") == 2);
+
+    assert(res.fieldNames() == ["id", "name", "date"]);
+
+    assert(res.front["id"] == "1");
+    assert(res.front["name"] == "Paul");
+    assert(res.front["date"] == "1989-05-06");
+
+    try {
+        res.getFieldIndex("column_which_not_exists");
+    } catch (Exception e) {
+        assert(e.msg == "column_which_not_exists not in result");
+    }
 }
